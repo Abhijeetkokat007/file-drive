775b0c565d6185668bcae1586a898cfad16b314b
AbhijeetKokat <abhijeetkokat2003@gmail.com>
2024-01-10 16:57:16 +0530

    create some changes

--- server/index.js
+++ server/index.js
@@ -77,7 +77,7 @@ app.get("/", async (req, res) => {
 });
 
 if(process.env.NODE_ENV === "production"){
-    app.use(Express.static(path.join(__dirname, '..', 'client', 'build'))); 
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build'))); 
    
     app.get('*', (req, res) => {
      res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
